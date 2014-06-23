5b890d5c0f1b5c23b58babd3346adcadf202bfaf
Olivier <yves.olli@gmail.com>
2014-06-23 21:04:20 +0200

    z71: cleanup and update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,8 +1,10 @@
 # Boot animation
 TARGET_BOOTANIMATION_NAME := vertical-320x480
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+
+# Include GSM stuff
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit device configuration
@@ -12,8 +14,6 @@ $(call inherit-product, device/commtiva/z71/z71.mk)
 PRODUCT_DEVICE := z71
 PRODUCT_NAME := cm_z71
 PRODUCT_BRAND := commtiva
-PRODUCT_MODEL := Commtiva Z71
+PRODUCT_MODEL := z71
 PRODUCT_MANUFACTURER := Commtiva
-
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=commtiva_z71 BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/commtiva_z71/z71:4.0.4/Olivier$(BUILD_ID)/0.1:user/test-keys PRIVATE_BUILD_DESC="Olivier"
+PRODUCT_CHARACTERISTICS := phone
