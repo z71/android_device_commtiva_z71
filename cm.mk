6049d0164a6db75f835c784a82a38a0a65f7f4f0
Olivier <yves.olli@gmail.com>
2014-06-24 16:49:05 +0200

    z71: cleanup and update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,23 +1,25 @@
-# Prepare for new BootAnimation
-TARGET_BOOTANIMATION_NAME := vertical-320x480
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
 
+# Include GSM stuff
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit z71 device
 $(call inherit-product, device/commtiva/z71/z71.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-PRODUCT_NAME=z71 \
-BUILD_ID=IMM76D \
-BUILD_FINGERPRINT=google/soju/crespo:4.0.4/IMM76D/299849:user/release-keys \
-PRIVATE_BUILD_DESC="soju-user 4.0.4 IMM76D 299849 release-keys" 
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 PRODUCT_NAME := cm_z71
 PRODUCT_BRAND := commtiva
 PRODUCT_DEVICE := z71
 PRODUCT_RELEASE_NAME := z71
 PRODUCT_MANUFACTURER := Foxconn
+PRODUCT_MODEL := Commtiva Z71
 PRODUCT_CHARACTERISTICS := phone
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := CommtivaZ71
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+-include vendor/cyanogen/products/common_versions.mk
